4659aceca88592f5ad3815fc994c6acee0a31956
David Tran <davidttemail@gmail.com>
2020-01-28 18:55:34 -0700

    Condense instanced variables in setup method

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -5,34 +5,29 @@ require './lib/card.rb'
 
 class TurnTest < Minitest::Test
 
+  def setup
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @turn = Turn.new("Juneau", @card)
+  end
+
   def test_it_exists
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-    assert_instance_of Turn, turn
+    assert_instance_of Turn, @turn
   end
 
   def test_if_it_returns_card
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-    assert_equal card, turn.card
+    assert_equal @card, @turn.card
   end
 
   def test_guess_method
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-    assert_equal "Juneau", turn.guess
+    assert_equal "Juneau", @turn.guess
   end
 
   def test_if_guess_equals_answer
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-    assert_equal true, turn.correct?
+    assert_equal true, @turn.correct?
   end
 
   def test_feedback
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-    assert_equal "Correct!", turn.feedback
+    assert_equal "Correct!", @turn.feedback
   end
 
   def test_guess
