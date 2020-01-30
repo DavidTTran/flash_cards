3b583db061b41d5336bb362538edaeab83a4a608
David Tran <davidttemail@gmail.com>
2020-01-30 08:56:42 -0700

    Add test for 'round.method'

--- test/round_test.rb
+++ test/round_test.rb
@@ -10,9 +10,15 @@ class RoundTest < Minitest::Test
     @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    @deck = Deck.new([card_1, card_2, card_3])
-    @round = Round.new(deck)
+    @deck = Deck.new([@card_1, @card_2, @card_3])
+    @round = Round.new(@deck)
   end
 
-  def
+  def test_turns_method
+    assert_equal [], @round.turns
+  end
+
+  def test_if_it_exists
+    assert_instance_of Round, @round
+  end
 end
