d794dec2a02facb5a6728f83e771425d55249bcd
David Tran <davidttemail@gmail.com>
2020-02-01 13:33:17 -0700

    Remove comments

--- lib/card.rb
+++ lib/card.rb
@@ -7,22 +7,3 @@ class Card
     @category = category
   end
 end
-
-
-
-
-# # double check that you are in your flashcards project directory!
-# pry(main)> require './lib/card'
-# #=> true
-#
-# pry(main)> card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-# #=> #<Card:0x00007f800e29f0c8 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>
-#
-# pry(main)> card.question
-# #=> "What is the capital of Alaska?"
-#
-# pry(main)> card.answer
-# #=> "Juneau"
-#
-# pry(main)> card.category
-# #=> :Geography
